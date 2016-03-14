2c8b759f689a740df26069a7357a4289d30a6ba7
Fred Proctor <frederick.proctor@nist.gov>
2016-03-14 17:03:45 -0400

    added quaternion printing to matrix test

--- tools/cpp/crcl_cpp/src/matrix_test.cpp
+++ tools/cpp/crcl_cpp/src/matrix_test.cpp
@@ -7,8 +7,9 @@
 int main(int argc, char *argv[])
 {
   double r, p, y;
-  tf::Matrix3x3 m, mout;
+  tf::Matrix3x3 m;
   tf::Vector3 v1, v2, v3;
+  tf::Quaternion q;
 
   while (! feof(stdin)) {
 #define BUFFERLEN 256
@@ -27,9 +28,12 @@ int main(int argc, char *argv[])
     v2 = m.getColumn(1);
     v3 = m.getColumn(2);
 
+    // the rotation matrix, by visual columns
     printf("%f %f %f\n", v1.getX(), v2.getX(), v3.getX());
     printf("%f %f %f\n", v1.getY(), v2.getY(), v3.getY());
     printf("%f %f %f\n", v1.getZ(), v2.getZ(), v3.getZ());
+
+    // the rotation matrix, by matrix[row][column]
     printf("---\n");
     printf("%f %f %f\n", m[0][0], m[0][1], m[0][2]);
     printf("%f %f %f\n", m[1][0], m[1][1], m[1][2]);
@@ -37,6 +41,13 @@ int main(int argc, char *argv[])
     tf::Matrix3x3 mout(v1.getX(), v2.getX(), v3.getX(),
 		       v1.getY(), v2.getY(), v3.getY(),
 		       v1.getZ(), v2.getZ(), v3.getZ());
+
+    // the quaternion
+    q.setRPY(r, p, y);
+    printf("---\n");
+    printf("%f %f %f %f\n", q.getX(), q.getY(), q.getZ(), q.getW());
+
+    // check the output rpy
     mout.getRPY(r, p, y);
     printf("---\n");
     printf("%f %f %f\n", r, p, y);
