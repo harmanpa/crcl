ab0d04d5e7fea5aa38089f9f1e6ab46f45befee8
Will Shackleford <william.shackleford@nist.gov>
2016-03-21 11:00:57 -0400

    update runWebApp.sh script use tomcat

--- tools/java/crcl4java/runWebApp.sh
+++ tools/java/crcl4java/runWebApp.sh
@@ -9,23 +9,50 @@ if test "x" != "x${JAVA_HOME}" ; then
     export PATH="${JAVA_HOME}/bin/:${PATH}";
 fi
 
-export JARFILE=crcl4java-ui/target/crcl4java-ui-1.0-SNAPSHOT-jar-with-dependencies.jar
+JARFILE=`find . -name crcl4java-vaadin-webapp\*war-exec.jar | head -n 1`;
+
+if test "x${JARFILE}" = "x" ; then
+    export JARFILE=crcl4java-vaadin-webapp/target/crcl4java-vaadin-webapp-1.3-SNAPSHOT-war-exec.jar
+fi
+
 if test ! -f "${JARFILE}" ; then
     mvn -version || ( echo "Please install maven." && false)
-    mvn package
-    mvn jetty:help -Ddetail=true
+    mvn install
+    ( cd crcl4java-vaadin-webapp; mvn -Ptomcat_embed install ; cd ..)
+else 
+    echo "Found executable jar file already compiled. ${JARFILE}";
 fi;
 
 \rm -f run[0-9]*.log run[0-9]*.err >/dev/null 2>/dev/null || true
 
-mvn -version
-
-pids=`jps | grep crcl4java-ui | awk '{print $1}'`
+pids=`jps | grep crcl4java-vaadin-webapp | awk '{print $1}'`
 if test "x${pids}x" != "xx" ; then 
+    echo "Killing previously started copies of this application. ${pids}"
     kill -KILL ${pids};
 fi
 
-java -Dsimserver.logimages=true -jar "${JARFILE}" --mode GraphicalServer > run$$.log 2> run$$.err &
+echo "Running java -jar ${JARFILE}"
 
-( sleep 30 ; firefox http://localhost:8080/crcl4java-vaadin-webapp/ ) &
-( set -x; cd crcl4java-vaadin-webapp ; mvn jetty:stop >/dev/null 2>/dev/null || true ; mvn jetty:run ; (mvn jetty:stop >/dev/null 2>/dev/null) || true );
+java  -jar "${JARFILE}" > run$$.log 2> run$$.err &
+jpid=$!
+
+sleep 10
+tail -f run$$.log &
+taillogpid=$!
+tail -f run$$.err &
+tailerrpid=$!
+
+firefox http://localhost:8080/crcl4java-vaadin-webapp/ &
+
+sleep 1
+echo 
+echo "Press enter to kill the server"
+read var
+
+kill ${jpid} ${taillogpid} ${tailerrpid}
+
+pids=`jps | grep crcl4java-vaadin-webapp | awk '{print $1}'`
+if test "x${pids}x" != "xx" ; then 
+    echo "Killing previously started copies of this application. ${pids}"
+    kill -KILL ${pids};
+fi
