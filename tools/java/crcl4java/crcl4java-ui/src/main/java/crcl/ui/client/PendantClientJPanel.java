21db7a6ed93e23e0ff17da38ddfff22e5d3a4515
Will Shackleford <william.shackleford@nist.gov>
2016-12-13 09:53:13 -0500

    ignore non existant properties file on loadProperties

--- tools/java/crcl4java/crcl4java-ui/src/main/java/crcl/ui/client/PendantClientJPanel.java
+++ tools/java/crcl4java/crcl4java-ui/src/main/java/crcl/ui/client/PendantClientJPanel.java
@@ -458,7 +458,7 @@ public class PendantClientJPanel extends javax.swing.JPanel implements PendantCl
     public int getPort() {
         return Integer.parseInt(this.jTextFieldPort.getText());
     }
-    
+
     public void setPort(int port) {
         jTextFieldPort.setText(Integer.toString(port));
     }
@@ -736,13 +736,15 @@ public class PendantClientJPanel extends javax.swing.JPanel implements PendantCl
     }
 
     public void loadProperties() {
-        loadPrefsFile(propertiesFile);
+        if (null != propertiesFile && propertiesFile.exists()) {
+            loadPrefsFile(propertiesFile);
+        }
     }
-    
+
     public void saveProperties() {
         savePrefsFile(propertiesFile);
     }
-    
+
     private void loadPrefsFile(File f) {
         try {
             File crcljavaDir = new File(System.getProperty("user.home"), CRCLJAVA_USER_DIR);
@@ -2364,7 +2366,6 @@ public class PendantClientJPanel extends javax.swing.JPanel implements PendantCl
 //            Logger.getLogger(PendantClientJPanel.class.getName()).log(Level.SEVERE, null, ex);
 //        }
 //    }
-
     public void useExiAction() {
         if (this.isConnected()) {
             this.disconnect();
