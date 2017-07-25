3e1afd2f5074a922fe3d4358efeeac73ce926484
Will Shackleford <william.shackleford@nist.gov>
2017-07-25 15:24:25 -0400

    update rcslib version

--- tools/java/crcl4java/crcl4java-ui/setCrclUiEnvVars.sh
+++ tools/java/crcl4java/crcl4java-ui/setCrclUiEnvVars.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export crcl_ui_finalname="crcl4java-ui-1.4-SNAPSHOT";
-export crcl_ui_fullpath_jar="/mnt/ubuntu/home/shackle/usnistgov/crcl/tools/java/crcl4java/crcl4java-ui/target/crcl4java-ui-1.4-SNAPSHOT-jar-with-dependencies.jar";
-export crcl_ui_jar="crcl4java-ui-1.4-SNAPSHOT-jar-with-dependencies.jar";
-export crcl_ui_fullpath_javadoc_jar="/mnt/ubuntu/home/shackle/usnistgov/crcl/tools/java/crcl4java/crcl4java-ui/target/crcl4java-ui-1.4-SNAPSHOT-javadoc.jar";
-export crcl_ui_javadoc_jar="crcl4java-ui-1.4-SNAPSHOT-javadoc.jar";
+export crcl_ui_finalname="crcl4java-ui-1.4";
+export crcl_ui_fullpath_jar="/mnt/ubuntu/home/shackle/usnistgov/crcl/tools/java/crcl4java/crcl4java-ui/target/crcl4java-ui-1.4-jar-with-dependencies.jar";
+export crcl_ui_jar="crcl4java-ui-1.4-jar-with-dependencies.jar";
+export crcl_ui_fullpath_javadoc_jar="/mnt/ubuntu/home/shackle/usnistgov/crcl/tools/java/crcl4java/crcl4java-ui/target/crcl4java-ui-1.4-javadoc.jar";
+export crcl_ui_javadoc_jar="crcl4java-ui-1.4-javadoc.jar";
 export fullpath_javadoc_jar="${crcl_ui_fullpath_javadoc_jar}";
 export javadoc_jar="${crcl_ui_javadoc_jar}";
 export fullpath_jar="${crcl_ui_fullpath_jar}";
