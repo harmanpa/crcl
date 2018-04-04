57e2dcbf891bd5cb1d9a7f1940b26c80b9483512
Will Shackleford <william.shackleford@nist.gov>
2018-04-04 10:18:38 -0400

    don't add --startserver which seems to no longer work

--- tools/java/crcl4java/crcl4java-ui/src/main/command/run.bat
+++ tools/java/crcl4java/crcl4java-ui/src/main/command/run.bat
@@ -5,6 +5,6 @@ SET mypath=%~dp0
 
 SET crcl_ui_jar=${project.build.directory}\${project.build.finalName}-jar-with-dependencies.jar
 
-java -Dsimserver.logImages=true -jar %crcl_ui_jar% --startserver %*
+java -Dsimserver.logImages=true -jar %crcl_ui_jar% %*
 
 @REM %mypath%\..\run.bat
